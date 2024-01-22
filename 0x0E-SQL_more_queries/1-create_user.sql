9843a3727d11e4516595cb3d2efd583755227c5a
soufianechandad <132358732+soufianechandad@users.noreply.github.com>
2024-01-22 20:39:26 +0100

    Add files via upload

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Query that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
